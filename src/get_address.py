532c87a0646754c3130aca9b2054e5d395fa0561
Bai Liang <bailiangcn@gmail.com>
2011-01-20 20:13:25 +0800

    增加了基本的短信提交的处理功能

---
+++ src/get_address.py
@@ -0,0 +1,15 @@
+#!/usr/bin/env python
+# -*- coding: UTF-8 -*-
+# AUTHOR:  BaiLiang , bailiangcn@gmail.com
+# Last Change:  2011年01月20日 20时11分47秒
+
+
+"""docstring
+"""
+
+__revision__ = '0.1'
+
+
+print "I received the query string",REQUEST
+print "<br>servicephone:%s<br>userphone:%s<br>message:%s" %(_servicephone,_phone, _message)
+
