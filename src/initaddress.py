619a26459dccf9cbde5aedbbbb870dd62ec10d45
Bai Liang <bailiangcn@gmail.com>
2011-01-20 16:11:33 +0800

    完成xml文件的输出美化，完成xml区域id、楼号id的自动更新

--- src/initaddress.py
+++ src/initaddress.py
@@ -1,7 +1,7 @@
 #!/usr/bin/env python
 # -*- coding: UTF-8 -*-
 # AUTHOR:  BaiLiang , bailiangcn@gmail.com
-# Last Change:  2011-01-20 01:16:05
+# Last Change:  2011年01月20日 16时10分11秒
 
 
 """初始化xml文件，生成唯一id
@@ -17,31 +17,103 @@ def initarea():
     '''
         给区域area.xml排顺序id
     '''
-    filename = "../addressdata/area.xml"
-    domarea=xml.dom.minidom.parse(filename)
+    areafilename = "../addressdata/area.xml"
+    domarea=xml.dom.minidom.parse(areafilename)
     rootarea=domarea.documentElement
     reglist=rootarea.getElementsByTagName('regional')
+    regionalnum = 0
+    housenum = 0
     for reg in reglist: #区域nodes
-        print reg.getElementsByTagName("name")[0].firstChild.data.encode("utf-8")
-        print reg.getElementsByTagName("datafile")[0].firstChild.data.encode("utf-8")
-        print reg.getElementsByTagName("valid")[0].firstChild.data.encode("utf-8")
+        regionalnum += 1 
+        #给各个区域进行编码id = regionalnum
         regid = reg.getElementsByTagName("id")[0]
-        text = domarea.createTextNode("2000")
-        print "has id :%s" % (regid.hasChildNodes())
+        text = domarea.createTextNode(str(regionalnum))
         if regid.hasChildNodes():
-            regid.firstChild.data = "1999"
+            regid.firstChild.data = str(regionalnum)
         else:
             regid.appendChild(text)
+        communityfilename = reg.getElementsByTagName("datafile"
+                )[0].firstChild.data.encode("utf-8")
+        commfilename = ''.join(("../addressdata/", communityfilename, ".xml"))
+        housenum = inithouse(commfilename, housenum)
 
-    f=file(filename,'w')
+    f=file(areafilename,'w')
     writer=codecs.lookup('utf-8')[3](f)
     domarea.writexml(writer,encoding='utf-8')
     writer.close 
     return 1
 
+def inithouse(commfilename, housenum):
+    '''
+        根据输入的filename进行楼号Id的重新编排
+    '''
+    domcomm=xml.dom.minidom.parse(commfilename)
+    rootcomm=domcomm.documentElement
+    commlist=rootcomm.getElementsByTagName('community')
+    commnum = 0
+    for comm in commlist:
+        commnum += 1 
+        houselist = comm.getElementsByTagName('house')
+        for house in houselist:
+            housenum += 1
+            houseidlist = house.getElementsByTagName('id')
+            idtext = domcomm.createTextNode(str(housenum))
+            idelement = domcomm.createElement("id")
+            idelement.appendChild(idtext)
+            if houseidlist.length>0:  #如果有id元素
+                houseid = houseidlist[0]
+                if houseid.hasChildNodes():
+                    houseid.firstChild.data = str(housenum)
+                else:
+                    houseid.appendChild(idtext)
+            else:   #如果没有Id元素
+                house.appendChild(idelement)
+
+    #保存xml结果到文件
+    #遍历nodes，删除所有空格元素
+    domcopy = domcomm.cloneNode(True)
+    clearspace(domcopy, domcopy.documentElement)
+    Indent(domcopy, domcopy.documentElement)
+    f=file(commfilename,'w')
+    writer=codecs.lookup('utf-8')[3](f)
+    domcopy.writexml(writer,encoding='utf-8')
+    writer.close 
+
+    return housenum
+
+def clearspace(dom, node):
+    '''
+    删除xml文件中的所有空行
+    '''
+    children = node.childNodes[:]
+    if children:
+        for nd in children:
+            if nd.nodeType == 3 and nd.data.isspace(): 
+                node.removeChild(nd)
+            else:
+                clearspace(dom,nd)
+
+def Indent(dom, node, indent = 0):
+    # Copy child list because it will change soon
+    children = node.childNodes[:]
+    # Main node doesn't need to be indented
+    if indent:
+        text = dom.createTextNode('\n' + '\t' * indent)
+        node.parentNode.insertBefore(text, node)
+    if children:
+        # Append newline after last child, except for text nodes
+        if children[-1].nodeType == node.ELEMENT_NODE:
+            text = dom.createTextNode('\n' + '\t' * indent)
+            node.appendChild(text)
+        # Indent children which are elements
+        for n in children:
+            if n.nodeType == node.ELEMENT_NODE:
+                Indent(dom, n, indent + 1)
+
 #
 ##自动调用测试用例，请输入测试用例名
 #
+
 def testmain():
     import unittest
     import sys
