1f078548495e0fdf8589886ba0416d63e0702ade
Bai Liang <bailiangcn@gmail.com>
2011-02-09 15:50:30 +0800

    可以切换视频，准备测试生产环境

--- broadcast/schedule.py
+++ broadcast/schedule.py
@@ -20,12 +20,18 @@ class Frame(wx.Frame):
     '''
     节目播出窗口类
     '''
-    def __init__(self, parent, id, title, mplayer, media_file):
+    def __init__(self, parent, id, title, mplayer, media_file,x,y):
         wx.Frame.__init__(self, None, -1, u"无边框窗口", (winx, winy) ,(winw, winh), 
                 style = wx.FRAME_SHAPED|wx.SIMPLE_BORDER|wx.FRAME_NO_TASKBAR|wx.STAY_ON_TOP)
 
-        self.mpc = mpc.MplayerCtrl(self, -1, mplayer, keep_pause=True)
+        self.mpc = mpc.MplayerCtrl(self, -1, mplayer, media_file,keep_pause=False)
+        #self.mpc = mpc.MplayerCtrl(self, -1, mplayer, media_file,[u'-vo',u'xv'],keep_pause=False)
+        self.Bind(mpc.EVT_MEDIA_STARTED, self.on_media_started)
+        self.Show()
 
+    def on_media_started(self, evt):
+            print 'Media started!'
+            self.mpc.FrameStep()
 
 #---------------------------------------------------------------------------
 
@@ -35,27 +41,40 @@ class ParFrame(wx.Frame):
     '''
 
     def __init__(self):
-        wx.Frame.__init__(self, None,  -1, 'Farther')
+        wx.Frame.__init__(self, None,  -1, 'Manager')
         self.win = None
+        self.win1=None
         panel = wx.Panel(self, -1)
         b = wx.Button(panel, -1, u"打开播放窗口", (50,50))
         b.SetPosition((15, 15))
         self.Bind(wx.EVT_BUTTON, self.OnButtonb, b)
 
-        c = wx.Button(panel, -1, u"预先调入", (50,50))
+        c = wx.Button(panel, -1, u"播放", (50,50))
         c.SetPosition((15, 55))
         self.Bind(wx.EVT_BUTTON, self.OnButtonc, c)
 
-        d = wx.Button(panel, -1, u"开始播放", (50,50))
+        d = wx.Button(panel, -1, u"切换插播", (50,50))
         d.SetPosition((15, 95))
         self.Bind(wx.EVT_BUTTON, self.OnButtond, d)
 
-        e = wx.Button(panel, -1, u"播放新视频", (50,50))
+        b1 = wx.Button(panel, -1, u"打开插播窗口", (50,50))
+        b1.SetPosition((115, 15))
+        self.Bind(wx.EVT_BUTTON, self.OnButtonb1, b1)
+
+        c1 = wx.Button(panel, -1, u"预留", (50,50))
+        c1.SetPosition((115, 55))
+        self.Bind(wx.EVT_BUTTON, self.OnButtonc1, c1)
+
+        d1 = wx.Button(panel, -1, u"预留", (50,50))
+        d1.SetPosition((115, 95))
+        self.Bind(wx.EVT_BUTTON, self.OnButtond1, d1)
+
+        e = wx.Button(panel, -1, u"预留", (50,50))
         e.SetPosition((15, 135))
         self.Bind(wx.EVT_BUTTON, self.OnButtone, e)
 
         button = wx.Button(panel, 1003, u"退出")
-        button.SetPosition((115, 15))
+        button.SetPosition((215, 15))
         self.Bind(wx.EVT_BUTTON, self.OnCloseMe, button)
         self.Bind(wx.EVT_CLOSE, self.OnCloseWindow)
 
@@ -63,20 +82,34 @@ class ParFrame(wx.Frame):
 
 
     def OnButtonb(self, evt):
-        self.win = Frame(None, -1, 'Hello MplayerCtrl', u'mplayer', u'2.mpg')
-        self.win.Show(True)
+        self.win = Frame(None, -1, 'Hello MplayerCtrl', u'mplayer', u'2.mpg',1920,0)
 
     def OnButtonc(self, evt):
-        self.win.mpc.Loadfile(u'2.mpg')
-        #if not self.win.mpc.pause:
         self.win.mpc.Pause()
-        self.win.mpc.Seek(0,2)
 
     def OnButtond(self, evt):
-        self.win.mpc.keep_pause=False
-        if self.win.mpc.pause:
+        if self.win.IsShown():
+            self.win1.mpc.Pause()
+            self.win1.Show()
             self.win.mpc.Pause()
-            self.win.mpc.keep_pause=True
+            self.win.Hide()
+        else:
+            self.win.mpc.Pause()
+            self.win.Show()
+            self.win1.mpc.Pause()
+            self.win1.Hide()
+
+    def OnButtonb1(self, evt):
+        self.win1 = Frame(None, -1, 'Hello MplayerCtrl', u'mplayer', u'1.mpg',2120,0)
+
+    def OnButtonc1(self, evt):
+        print self.win.mpc.get
+
+    def OnButtond1(self, evt):
+        self.win1.mpc.keep_pause=False
+        if self.win1.mpc.pause:
+            self.win1.mpc.Pause()
+            self.win1.mpc.keep_pause=True
 
     def OnButtone(self, evt):
         self.win.mpc.Mute()
@@ -93,6 +126,9 @@ class ParFrame(wx.Frame):
     def OnCloseWindow(self, event):
         if self.win != None:
             self.win.Destroy()
+        if self.win1 != None:
+            self.win1.Destroy()
+
         self.Destroy()
 
         
