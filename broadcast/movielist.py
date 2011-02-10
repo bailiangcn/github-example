f0d3de281f7c42646033b228161c0c2c2d917fb6
Bai Liang <bailiangcn@gmail.com>
2011-02-10 22:33:07 +0800

    完成节目列表类的基本功能

--- broadcast/movielist.py
+++ broadcast/movielist.py
@@ -16,8 +16,99 @@
 
 __revision__ = '0.1'
 
+import xml.dom.minidom
+import glob
+import os
+
+class Mlist(object):
+    '''
+    影片列表类
+    '''
+    def __init__(self,filename=u'mlist.xml'):
+        '''
+        类的初始化,读取一个xml文件,生成文件列表
+        '''
+        self.mlist=[]   #播出文件列表
+        self.length=0   #播出文件的个数
+        self.nowplay=0  #当前播放的影片号,从0开始
+        dom=xml.dom.minidom.parse(filename)
+        self.root=dom.documentElement
+        self.length=len(self.root.getElementsByTagName('movie'))
+        if self.length > 0:
+            movielist = self.root.getElementsByTagName('movie')
+            for eachmovie in movielist:
+                self.mlist.append(getText(eachmovie))
+        return 
+
+    def append(self,moviename,order=-9):
+        '''
+        插入一个影片到末尾,0表示最前面,-9表示最后面,
+        -1表示插入正在播放的后面
+        '''
+        if order == -1:
+            order=self.length+1
+        if order >= 0 and order <self.length:
+            self.mlist.insert(order,moviename)
+        else :
+            self.mlist.append(moviename)
+        self.length += 1
+        return
+
+    def savefile(self,filename):
+        '''
+        保存节目列表为xml文件
+        '''
+        data = []
+        data.append(u'<?xml version="1.0" encoding="utf-8"?>')
+        data.append(u'<ROOT>')
+        for eachmovie in self.mlist:
+            data.append(''.join((u' '*4 + u'<movie>',
+                eachmovie,u'</movie>')))
+        data.append(u'</ROOT>')
+
+        try:
+            f = open(filename, 'w')
+            for line in data:
+                line += '\n' 
+                f.write(line.encode('utf-8'))
+            f.close()
+        except Exception, ex:  
+            #如果错误, 记入日志
+            print ex
+            print sys.exc_info()
+    def getnextfile(self):
+        '''
+        返回下一个影片的文件名
+        '''
+        if self.nowplay == self.length-1:
+            return self.mlist[0]
+        else:
+            return self.mlist[self.nowplay+1]
+
+    def makebaselist(self,path,extension=['*']):
+        '''
+        根据指定目录增加所有文件到播放列表,如果重复则不添加
+        '''
+        for eachex in extension:
+            tmppath=os.path.join(path,eachex)
+            filelist=glob.glob(tmppath)
+            for eachfile in filelist:
+                if eachfile not in self.mlist:
+                    self.mlist.append(eachfile)
+        self.length=len(self.mlist)
+        
 
 
+def getText(node):
+    '''
+    读取一个只包含text元素的node值, 返回其中的内容
+    '''
+    rc = ""
+    
+    for eachnode in node.childNodes:
+        if eachnode.nodeType == eachnode.TEXT_NODE:
+            rc = rc + eachnode.data
+    return rc
 
 #
 ##自动调用测试用例，请输入测试用例名
--- broadcast/tests/testmovielist.py
+++ broadcast/tests/testmovielist.py
@@ -18,20 +18,55 @@ sys.path.append(os.curdir)
 sys.path.append(os.path.join(os.pardir, ''))
 
 from unittest import TestCase
-import movielist
+from movielist import *
 
 class simpleTest(TestCase):
     def setUp(self):
-        pass
+        self.mt=Mlist('mlist.xml')
 
     def tearDown(self):
         pass
 
-    def testExample(self):
-        self.assertEqual(1, 1)
+    def testInit(self):
+        "测试类的初始化"
+        self.mt.mlist=[u'1.mpg', u'2.mpg']
+        knownValues=[u'1.mpg', u'2.mpg']
+        self.assertEqual(self.mt.mlist, knownValues)
+        self.assertEqual(2, self.mt.length)
 
-    def testOther(self):
-        self.assertNotEqual(0, 1)
+    def testAppend(self):
+        "测试增加一个节目"
+        knownValues=(
+                ('3.mpg',0,[u'3.mpg',u'1.mpg', u'2.mpg']),
+                ('3.mpg',1,[u'1.mpg',u'3.mpg', u'2.mpg']),
+                ('3.mpg',-9,[u'1.mpg', u'2.mpg',u'3.mpg']),
+                )
+        for mn, od, res in knownValues:
+            self.mt.mlist=[u'1.mpg', u'2.mpg']
+            self.mt.length=2
+            self.mt.append(mn,od)
+            self.assertEqual(res,self.mt.mlist)
+            self.assertEqual(3, self.mt.length)
+
+    def testSavefile(self):
+        "测试根据列表生成xml文件"
+        self.mt.mlist=[u'1.mpg', u'2.mpg']
+        self.mt.savefile(u'mlist.xml')
+
+    def testgetnextfile(self):
+        "测试返回下一个播出文件"
+        self.mt.mlist=[u'4.mpg', u'2.mpg']
+        self.mt.nowplay=0
+        self.mt.length=2
+        self.assertEqual(u'2.mpg',self.mt.getnextfile())
+        self.mt.nowplay=1
+        self.assertEqual(u'4.mpg',self.mt.getnextfile())
+
+    def testmakebaselist(self):
+        "测试根据指定目录生成节目列表" 
+        self.mt.makebaselist('',['*.py','*.xml'])
+        print self.mt.mlist
+
 
 if '__main__' == __name__:
 
