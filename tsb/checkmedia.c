e441d32286977883b8cc310a6c9901a75c51557d
bailiang <bailiangcn@gmail.com>
2011-02-27 00:09:38 +0800

    完成pat表格解析

--- tsb/checkmedia.c
+++ tsb/checkmedia.c
@@ -18,7 +18,7 @@
 #include <string.h>
 #include <stdlib.h>
 
-#define BUFFERLEN 1024
+#define BUFFERLEN 1880
 #define SYNTIMES 3
 
 /* 
@@ -67,10 +67,103 @@ void check188or204(int bsize, unsigned char *temp, int *res)
 	return;
 }
 
-void printts(int bsize, unsigned char *data)
+void fb(unsigned c)
+{
+	for (int i = 0; i < 8; i++) {
+		if (i % 4 == 0)
+			printf(" ");
+		putchar(((c & 1 << 7) == 0) ? '0' : '1');
+		c <<= 1;
+	}
+}
+
+/* 
+ * ===  FUNCTION  ======================================================================
+ *         Name:  print_ts
+ *  Description:  用16进制方式显示二进制数据
+ * =====================================================================================
+ */
+void print_ts(int bsize, unsigned char *data)
 {
 	for (int i = 0; i < bsize; i++) {
-		printf("%x ", data[i]);
+		if (data[i] != 0xff) {
+			printf("%d==>%x (", i, data[i]);
+			fb(data[i]);
+			printf(")\n");
+		}
+	}
+	printf("\n");
+}
+
+void split_ts(int bsize, unsigned char *data)
+{
+	struct ts_first {
+		unsigned int transport_error_indicator:1;
+		unsigned int payload_unit_start_indicator:1;
+		unsigned int transport_priority:1;
+		unsigned long int pid:13;
+		unsigned int transport_scrambling_control:2;
+		unsigned int adaption_field_control:2;
+		unsigned int continuity_counter:4;
+	} tsp;
+	unsigned char tempindi = 0;
+	printf("解析开始:\n");
+	if (data[0] == 0x47) {
+		printf("同步正常\n");
+	} else {
+		printf("同步失败\n");
+		return;
+	}
+	tsp.transport_error_indicator = (data[1] & (1 << 7)) ? 1 : 0;
+	tsp.payload_unit_start_indicator = (data[1] & (1 << 6)) ? 1 : 0;
+	tsp.transport_priority = (data[1] & (1 << 5));
+	tsp.pid = data[1] & 0x1F;
+	tsp.pid = tsp.pid << 8 + data[2];
+	tsp.transport_scrambling_control = ((data[3] >> 6) & 0x3);
+	tsp.adaption_field_control = ((data[3] >> 4) & 0x3);
+	tsp.continuity_counter = (data[3] & 0xF);
+
+	printf("transport_error_indicator:%d\n", tsp.transport_error_indicator);
+	printf("payload_unit_start_indicator:%d\n",
+	       tsp.payload_unit_start_indicator);
+	printf("tarnsport_priority:%d\n", tsp.transport_priority);
+	printf("tarnsport_pid:0x%x\n", tsp.pid);
+	printf("transport_scrambling_control:%d\n",
+	       tsp.transport_scrambling_control);
+	printf("adaption_field_control:%d\n", tsp.adaption_field_control);
+	printf("continuity_counter:%d\n", tsp.continuity_counter);
+
+	if (tsp.pid == 0x00) {
+		printf("pat 表格:\n");
+		unsigned long int tempval = 0, tempvala;
+		tempval = data[4];
+		printf("pointer_field:%ld\n", tempval);
+		tempval = data[5];
+		printf("table_id:%lx\n", tempval);
+		tempval = (data[6] & (1 << 7)) ? 1 : 0;
+		printf("section_syntax_indicator:%lx\n", tempval);
+		tempval = (data[6] & 0x0F) << 8;
+		tempval = tempval + data[7];
+		printf("section_length:%ld\n", tempval);
+		tempval = (data[8] << 8) + data[9];
+		printf("transport_stream_id:%ld\n", tempval);
+		tempval = (data[10] >> 1) & 0x1F;
+		printf("version_number:%ld\n", tempval);
+		tempval = data[10] & 0x1;
+		printf("current_next_indicator:%ld\n", tempval);
+		tempval = data[11];
+		printf("section_number:%ld\n", tempval);
+		tempval = data[12];
+		printf("last_section_number:%ld\n", tempval);
+		tempval = (data[13] << 8) + data[14];
+		printf("program_number:%ld\n", tempval);
+		tempvala = ((data[15] & 0x1F) << 8) + data[16];
+		if (tempval == 0x00)
+			printf("network_PID:%ld\n", tempvala);
+		else
+			printf("program_map_PID:%ld\n", tempvala);
+		printf("CRC:0x%x%x%x%x", data[17], data[18], data[19],
+		       data[20]);
 	}
 }
 
@@ -83,11 +176,14 @@ void printts(int bsize, unsigned char *data)
 int main(int argc, char *argv[])
 {
 	FILE *fp;
-	unsigned char *p, temp[BUFFERLEN] = { 0 };
+	unsigned char *p, temp[BUFFERLEN] = {
+		0
+	};
 	int offset_packet, bsize = 0;
 	char filename[256] = "1.ts";
-	int syn[2] = { 0 };
-
+	int syn[2] = {
+		0
+	};
 	if (argc == 2) {
 		strcpy(filename, argv[1]);
 	}
@@ -99,7 +195,6 @@ int main(int argc, char *argv[])
 
 	bsize = fread(temp, sizeof(unsigned char), sizeof(temp), fp);
 	printf("%s的%d字节被读取\n", filename, bsize);
-
 	if (bsize > 188) {
 		check188or204(bsize, temp, syn);
 		switch (syn[0]) {
@@ -111,9 +206,10 @@ int main(int argc, char *argv[])
 			printf("找到%d同步符号,起始位置:%d \n",
 			       syn[0], syn[1]);
 			offset_packet = 0;
+			printf("第%d个包:\n", offset_packet);
 			p = &temp[syn[1] + syn[0] * offset_packet];
-			printts(syn[0], p);
-
+			print_ts(syn[0], p);
+			split_ts(syn[0], p);
 			break;
 		}
 	}
