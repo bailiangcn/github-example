91deb4b3c4a4b54bcba52d964ebf9c1c6bfc1deb
bailiang <bailiangcn@gmail.com>
2011-02-27 20:40:11 +0800

    重构了代码，尚未完成get_ts_pocket

--- tsb/checkmedia.c
+++ tsb/checkmedia.c
@@ -1,5 +1,5 @@
 /*
- * =====================================================================================
+ * ==========================================================================
  *
  *       Filename:  checkmedia.c
  *
@@ -11,18 +11,17 @@
  *       Compiler:  gcc
  *
  *         Author:  bailiangcn@gmail.com
- * =====================================================================================
+ * ==========================================================================
  */
 
 #include <stdio.h>
 #include <string.h>
 #include <stdlib.h>
-
-#define BUFFERLEN 1880
-#define SYNTIMES 3
+#include <stdbool.h>
+#include "checkmedia.h"
 
 /* 
- * ===  FUNCTION  ======================================================================
+ * ===  FUNCTION  ===========================================================
  *         Name:  check188or204
  *  Description:  判断一个数组是否是188或者204格式
  *                bsize 数组的长度
@@ -31,7 +30,7 @@
  *				  res[0] = 0 文件未检测到同步
  *				  res[0] = 188 检测到188同步,起始位置 res[1]
  *				  res[0] = 204 检测到204同步,起始位置 res[1]
- * =====================================================================================
+ * ==========================================================================
  */
 void check188or204(int bsize, unsigned char *temp, int *res)
 {
@@ -67,6 +66,12 @@ void check188or204(int bsize, unsigned char *temp, int *res)
 	return;
 }
 
+/* 
+ * ===  FUNCTION  ===========================================================
+ *         Name:  fb
+ *  Description:  二进制显示一个8位的数字
+ * ==========================================================================
+ */
 void fb(unsigned c)
 {
 	for (int i = 0; i < 8; i++) {
@@ -78,10 +83,10 @@ void fb(unsigned c)
 }
 
 /* 
- * ===  FUNCTION  ======================================================================
+ * ===  FUNCTION  ===========================================================
  *         Name:  print_ts
  *  Description:  用16进制方式显示二进制数据
- * =====================================================================================
+ * ==========================================================================
  */
 void print_ts(int bsize, unsigned char *data)
 {
@@ -95,25 +100,27 @@ void print_ts(int bsize, unsigned char *data)
 	printf("\n");
 }
 
-void split_ts(int bsize, unsigned char *data)
+/* 
+ * ===  FUNCTION  ===========================================================
+ *         Name:  split_ts
+ *  Description:  解析一个ts包,返回一个包头结构
+ * ==========================================================================
+ */
+struct ts_packet split_ts(int bsize, unsigned char *data)
 {
-	struct ts_first {
-		unsigned int transport_error_indicator:1;
-		unsigned int payload_unit_start_indicator:1;
-		unsigned int transport_priority:1;
-		unsigned long int pid:13;
-		unsigned int transport_scrambling_control:2;
-		unsigned int adaption_field_control:2;
-		unsigned int continuity_counter:4;
-	} tsp;
+	struct ts_packet tsp;
 	unsigned char tempindi = 0;
 	printf("解析开始:\n");
 	if (data[0] == 0x47) {
-		printf("同步正常\n");
+		tsp.sync_byte = 0x47;
 	} else {
 		printf("同步失败\n");
-		return;
+		tsp.sync_byte = 0;
+		return tsp;
 	}
+	/* 
+	 * 对包头部分进行解析
+	 */
 	tsp.transport_error_indicator = (data[1] & (1 << 7)) ? 1 : 0;
 	tsp.payload_unit_start_indicator = (data[1] & (1 << 6)) ? 1 : 0;
 	tsp.transport_priority = (data[1] & (1 << 5));
@@ -122,7 +129,57 @@ void split_ts(int bsize, unsigned char *data)
 	tsp.transport_scrambling_control = ((data[3] >> 6) & 0x3);
 	tsp.adaption_field_control = ((data[3] >> 4) & 0x3);
 	tsp.continuity_counter = (data[3] & 0xF);
+	/* 
+	 * 对实际携带数据进行解析
+	 */
+	return tsp;
+}
 
+/* 
+ * ===  FUNCTION  ============================================================
+ *         Name:  ana_ts_pat
+ *  Description:  解析pat表格
+ * ===========================================================================
+ */
+struct ts_pat_packet ana_ts_pat(int bsize, unsigned char *data)
+{
+
+	struct ts_packet tsp;
+	struct ts_pat_packet ts_pat;
+
+	tsp = split_ts(bsize, data);
+	if (tsp.pid == 0x00) {
+		ts_pat.pointer_field = data[4];
+		ts_pat.table_id = data[5];
+		ts_pat.section_syntax_indicator = (data[6] & (1 << 7)) ? 1 : 0;
+		ts_pat.section_length = (data[6] & 0x0F) << 8;
+		ts_pat.section_length = ts_pat.section_length + data[7];
+		ts_pat.transport_stream_id = (data[8] << 8) + data[9];
+		ts_pat.version_number = (data[10] >> 1) & 0x1F;
+		ts_pat.current_next_indicator = data[10] & 0x1;
+		ts_pat.section_number = data[11];
+		ts_pat.last_section_number = data[12];
+		ts_pat.program_number = (data[13] << 8) + data[14];
+		ts_pat.np_pid = ((data[15] & 0x1F) << 8) + data[16];
+/* 		printf("CRC:0x%02x%02x%02x%02x\n", data[17], data[18], data[19],
+ * 		       data[20]);
+ */
+	} else {
+		ts_pat.table_id = 0xff;
+	}
+	return ts_pat;
+}
+
+/* 
+ * ===  FUNCTION  ============================================================
+ *         Name:  print_ts_head
+ *  Description:  打印ts表头结构
+ * ===========================================================================
+ */
+void print_ts_head(struct ts_packet tsp)
+{
+
+	printf("ts表头结构:\n");
 	printf("transport_error_indicator:%d\n", tsp.transport_error_indicator);
 	printf("payload_unit_start_indicator:%d\n",
 	       tsp.payload_unit_start_indicator);
@@ -132,88 +189,45 @@ void split_ts(int bsize, unsigned char *data)
 	       tsp.transport_scrambling_control);
 	printf("adaption_field_control:%d\n", tsp.adaption_field_control);
 	printf("continuity_counter:%d\n", tsp.continuity_counter);
-
-	if (tsp.pid == 0x00) {
-		printf("pat 表格:\n");
-		unsigned long int tempval = 0, tempvala;
-		tempval = data[4];
-		printf("pointer_field:%ld\n", tempval);
-		tempval = data[5];
-		printf("table_id:%lx\n", tempval);
-		tempval = (data[6] & (1 << 7)) ? 1 : 0;
-		printf("section_syntax_indicator:%lx\n", tempval);
-		tempval = (data[6] & 0x0F) << 8;
-		tempval = tempval + data[7];
-		printf("section_length:%ld\n", tempval);
-		tempval = (data[8] << 8) + data[9];
-		printf("transport_stream_id:%ld\n", tempval);
-		tempval = (data[10] >> 1) & 0x1F;
-		printf("version_number:%ld\n", tempval);
-		tempval = data[10] & 0x1;
-		printf("current_next_indicator:%ld\n", tempval);
-		tempval = data[11];
-		printf("section_number:%ld\n", tempval);
-		tempval = data[12];
-		printf("last_section_number:%ld\n", tempval);
-		tempval = (data[13] << 8) + data[14];
-		printf("program_number:%ld\n", tempval);
-		tempvala = ((data[15] & 0x1F) << 8) + data[16];
-		if (tempval == 0x00)
-			printf("network_PID:%ld\n", tempvala);
-		else
-			printf("program_map_PID:%ld\n", tempvala);
-		printf("CRC:0x%02x%02x%02x%02x", data[17], data[18], data[19],
-		       data[20]);
-	}
 }
 
 /* 
- * ===  FUNCTION  ======================================================================
- *         Name:  main
- *  Description:  主调用程序
- * =====================================================================================
+ * ===  FUNCTION  ============================================================
+ *         Name:  print_pat_head
+ *  Description:  打印pat表格结构
+ * ===========================================================================
  */
-int main(int argc, char *argv[])
+void print_pat_head(struct ts_pat_packet ts_pat)
 {
-	FILE *fp;
-	unsigned char *p, temp[BUFFERLEN] = {
-		0
-	};
-	int offset_packet, bsize = 0;
-	char filename[256] = "1.ts";
-	int syn[2] = {
-		0
-	};
-	if (argc == 2) {
-		strcpy(filename, argv[1]);
-	}
-	fp = fopen(filename, "rb");
-	if (fp == NULL) {
-		printf("文件打开失败,可能文件不存在\n");
-		return -1;
-	}
+	printf("pat 表格:\n");
+	printf("pointer_field:%d\n", ts_pat.pointer_field);
+	printf("table_id:%x\n", ts_pat.table_id);
+	printf("section_syntax_indicator:%x\n",
+	       ts_pat.section_syntax_indicator);
+	printf("section_length:%d\n", ts_pat.section_length);
+	printf("transport_stream_id:%d\n", ts_pat.transport_stream_id);
+	printf("version_number:%d\n", ts_pat.version_number);
+	printf("current_next_indicator:%d\n", ts_pat.current_next_indicator);
+	printf("section_number:%d\n", ts_pat.section_number);
+	printf("last_section_number:%d\n", ts_pat.last_section_number);
+	printf("program_number:%d\n", ts_pat.program_number);
+	if (ts_pat.program_number == 0x00)
+		printf("network_PID:%d\n", ts_pat.np_pid);
+	else
+		printf("program_map_PID:%d\n", ts_pat.np_pid);
+}
 
-	bsize = fread(temp, sizeof(unsigned char), sizeof(temp), fp);
-	printf("%s的%d字节被读取\n", filename, bsize);
-	if (bsize > 188) {
-		check188or204(bsize, temp, syn);
-		switch (syn[0]) {
-		case 0:
-			printf("未找到同步\n");
-			break;
-		case 188:
-		case 204:
-			printf("找到%d同步符号,起始位置:%d \n",
-			       syn[0], syn[1]);
-			offset_packet = 0;
-			printf("第%d个包:\n", offset_packet);
-			p = &temp[syn[1] + syn[0] * offset_packet];
-			print_ts(syn[0], p);
-			split_ts(syn[0], p);
-			break;
-		}
-	}
-
-	fclose(fp);
+/* 
+ * ===  FUNCTION  ============================================================
+ *         Name:  get_ts_packet
+ *  Description:  返回指定顺序的ts的包,如果不在内存当中自动读文件
+ *		返回值： -1 表示没有找到合适的包
+ *				 0  表示找到了合适的包
+ * ===========================================================================
+ */
+int get_ts_packet(unsigned long long int *pf_position, int order, int *res,
+		  unsigned char *temp, FILE * fp)
+{
+	printf("beging get ts packet");
 	return 0;
 }
