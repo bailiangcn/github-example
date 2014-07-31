69d45f83a6b6b05f661884f5dfb2020d686899b9
Bailiang <Bailiangcn@gmail.com>
2014-07-31 22:09:56 +0800

    调整了日志报告的格式

--- weather/autowea.py
+++ weather/autowea.py
@@ -22,9 +22,9 @@ import ConfigParser
 #表示生成的优先选用顺序
 ############################################
 
-cf=ConfigParser.ConfigParser()
+cf = ConfigParser.ConfigParser()
 cf.read("config.ini")
-ORDER = cf.get("host","order").decode('utf-8').split(' ')
+ORDER = cf.get("host", "order").decode('utf-8').split(' ')
 
 #ORDER = [u'0', u'9', u'1', u'2']
 #管理员邮箱, 可自行添加(但注意可能出现126认为是垃圾邮件)
@@ -34,7 +34,7 @@ MAILLIST = ['bailiangcn@163.com', 'test2011@126.com']
 #删除所有原有的wea*.xml文件
 os.system('rm template/wea[0-9].xml')
 
-mess = u'信息采集开始:\n'
+mess = u'信息采集开始:\n\n'
 for order in ORDER:
     cmdstr = u'weather.getWeather'+order+u'()'
     weafilename = u'template/wea'+order+u'.xml'
@@ -43,13 +43,13 @@ for order in ORDER:
     while i < 3:
         exec cmdstr
         if os.path.isfile(weafilename):
-            mess += u'信息源'+order+u'号成功\n'
+            mess = ''.join((mess, u' '*4, u'信息源', order, u'号成功\n'))
             i = 9
         else:
             time.sleep(5)
             i += 1
     if i < 9:
-        mess += u'信息源'+order+u'号失败\n'
+        mess = ''.join((mess, u' '*4, u'信息源', order, u'号失败\n'))
 
 #取出所有生成的中间xml文件
 allwealist = glob.glob('template/wea[0-9].xml')
@@ -60,25 +60,33 @@ if len(allwealist) == 0:
     sys.exit()
 else:
     #生成采集信息报告
-    mess += u'\n采集报告结果报告:'
+    mess = ''.join((mess, u'\n采集报告结果报告:\n', u'='*16))
     for order in ORDER:
         filename = u'template/wea' + order + u'.xml'
         if filename in allwealist:
-            mess = ''.join((mess, u'\n系统采集正常,信息来源:'))
             nowlist = weather.xmlToList(filename)
+            mess = ''.join((mess, u'\n\n系统采集正常,信息来源:', nowlist[0], u'\n'))
+            mess = ''.join((mess, u'\n', u' '*4, nowlist[1]))
             tempstr = "| ".join((
-                zhLjust(nowlist[0], 4), zhLjust(nowlist[1], 28),
-                zhLjust(nowlist[2], 20), zhLjust(nowlist[3], 12),
-                zhLjust(nowlist[4], 40), zhLjust(nowlist[5], 20),
+                zhLjust(nowlist[2], 20), zhLjust(nowlist[3], 20),
+                zhLjust(nowlist[5], 20), zhLjust(nowlist[4], 40),
+            ))
+            mess = ''.join((mess, u'\n', u' '*4, tempstr))
+            tempstr = "| ".join((
                 zhLjust(nowlist[6], 20), zhLjust(nowlist[7], 20),
-                zhLjust(nowlist[8], 20), zhLjust(nowlist[9], 20),
+                zhLjust(nowlist[8], 20),
+            ))
+            mess = ''.join((mess, u'\n', u' '*4, tempstr))
+            tempstr = "| ".join((
+                zhLjust(nowlist[9], 20),
                 zhLjust(nowlist[10], 20), zhLjust(nowlist[11], 20)
             ))
-            mess += tempstr
+            mess = ''.join((mess, u'\n', u' '*4, tempstr))
 for order in ORDER:
     filename = u'template/wea' + order + u'.xml'
     if filename in allwealist:
-        mess = ''.join((mess, u'\n系统采集正常,生成页面采用信息来源:', filename))
+        mess = ''.join((mess, u'\n\n', u'='*16,
+                        u'\n\n系统采集正常,生成页面采用信息来源: ', filename))
         weather.xmlToHtml(filename)
         weather.sendattachmail('bailiangcn@163.com',
                                u'天气预报运行报告',
